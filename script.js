4fb8888ccaea7f13bb42b9c5ce21efabc002d574
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-12-16 13:40:14 +0100

    Added more code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// STEGEN
 /*
 
 SET variabel mål till 1 // SET mål = 1 // Skapa variabel mål och sätt till 1
@@ -25,19 +27,63 @@ Loopa tills mål är större än 6
         Öka på varabel mål med 1
 Skriv ut vinstmeddelande till användare
 
+WHILE (mål > 6)
+    SET dice = random (1-6)
+    antalKast++;
+    IF(dice === mål)
+        mål++;
+    END IF
+END WHILE
+
 */
 
-let goal = 1;
-let nmbrOfThrows = 0;
+// let goal = 1;
+// let nmbrOfThrows = 0;
 
-while (goal < 7) {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    nmbrOfThrows++;
-    console.log(`Kast ${nmbrOfThrows}: ${dice}`);
-    if (dice === goal) {
-        goal++;
-        console.log('Träff!');
-    }
+// while (goal < 7) {
+//     let dice = Math.floor(Math.random() * 6) + 1;
+//     nmbrOfThrows++;
+//     console.log(`Kast ${nmbrOfThrows}: ${dice}`);
+//     if (dice === goal) {
+//         goal++;
+//         console.log('Träff!');
+//     }
+// }
+
+// console.log(`Grattis du vann på ${nmbrOfThrows} kast!`);
+
+// Söka efter likheter mellan ord
+/*
+
+SET variabel firstWord till INPUT från användaren
+SET variabel secondWord till INPUT från användaren
+SET variabel matchCount till 0;
+
+IF firstWord och secondWord har olika många bokstäver
+    PRINT Båda orden måste innehålla lika många bokstäver
+END IF
+
+FOR varje tecken i startWord, räkna med index `i`
+    IF firstWord plats i är lika med secondWord plats i // IF firstWord[i] === secondWord[i] Funkar också!
+        Räkna upp variabel matchCount med 1
+    End IF
+END FOR
+
+PRINT Det fanns matchCount likheter mellan orden
+
+*/
+
+let firstWord = prompt('Skriv in första ordet');
+let secondWord = prompt('Skriv in andra ordet');
+let matchCount = 0;
+
+if (firstWord.length !== secondWord.length) {
+    console.log('Båda orden måste innehålla lika många bokstäver');
 }
 
-console.log(`Grattis du vann på ${nmbrOfThrows} kast!`);
+for (let i = 0; i < firstWord.length; i++) {
+    if (firstWord[i] === secondWord[i]) {
+        matchCount++;
+    }
+}
+alert(`Det fanns ${matchCount} likheter mellan orden`);
